686da89b6db2e8c7674afa9b7b6df14ce7afa93d
Travis <tngraham2004@gmail.com>
2025-04-20 19:19:15 +1000

    Add text splitting to file_processor.py

--- file_processor.py
+++ file_processor.py
@@ -3,6 +3,8 @@ from langchain.schema import Document
 from env import GEMINI_API_KEY
 from google import genai
 from google.genai import types
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
 
 from custom_loader import GeminiLoader
 
@@ -21,10 +23,25 @@ class ObsidianProcessor:
         # Prep Gemini for OCR
         self._gemini = genai.Client(api_key=GEMINI_API_KEY)
 
+        self._pages = []
+        self._split_pages = []
 
-    def process_vault(self) -> List[Document]:
-        pages = self._loader.load()
-        return pages
+    def text_splitter(self, pages) -> None:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,  # chunk size (characters)
+            chunk_overlap=200,  # chunk overlap (characters)
+            add_start_index=True,  # track index in original document
+        )
+        self._split_pages = text_splitter.split_documents(pages)
+        print(self._split_pages)
+
+    def parse_vault(self):
+        self._pages = self._loader.load()
+
+    def process_vault(self) -> None:
+        self.parse_vault()
+        self.text_splitter(self._pages)
+
 
 
     # def ocr_pdf(self, file, source) -> Document:
